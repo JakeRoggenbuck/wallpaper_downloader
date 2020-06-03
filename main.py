c6166b8a4376d00b7b7b80d1a3eb5c91413ded9f
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-06-02 23:44:44 -0700

    Add image number

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ response = urllib.request.urlopen(url)
 page = response.read().decode("UTF-8")
 lines = page.split("\n")
 
-def image_download(local_filename):
+def image_download(local_filename, file_number, lenght):
     image_url = f'{main_url}{local_filename}'
     with requests.get(image_url, stream=True) as r:
         r.raise_for_status()
@@ -17,9 +17,9 @@ def image_download(local_filename):
             for chunk in r.iter_content(chunk_size=8192):
                 if chunk:
                     f.write(chunk)
-        print(f"Downloaded {save_name}")
+        print(f"[{file_number}/{lenght}] Downloaded {save_name}")
 
-for x, line in enumerate(lines):
-    if 'href="/download' in line and 'data-id' in line:
-        file_name = line.split('"')[1]
-        image_download(file_name)
+image = [line for line in lines if 'href="/download' in line and 'data-id' in line]
+for x, image in enumerate(image):
+    file_name = image.split('"')[1]
+    image_download(file_name, x, len(image))
